23025c8c760d09da13f966f748fc5c01b1c93cf5
jiangxincode <jiangxinnju@163.com>
2015-08-24 22:43:58 +0800

    temp

--- src/test/java/com/shengsiyuan/chat/util/XMLUtilTest.java
+++ src/test/java/com/shengsiyuan/chat/util/XMLUtilTest.java
@@ -10,38 +10,32 @@ import org.dom4j.io.SAXReader;
 import org.junit.Before;
 import org.junit.Test;
 
-public class XMLUtilTest
-{
+public class XMLUtilTest {
 	private SAXReader saxReader;
-	
+
 	@Before
-	public void setUp()
-	{
+	public void setUp() {
 		saxReader = new SAXReader();
 	}
-	
+
 	@Test
-	public void testConstructLoginXML()
-	{
-		try
-		{
+	public void testConstructLoginXML() {
+		try {
 			String xml = XMLUtil.constructLoginXML("zhangsan");
-			
+
 			Document document = saxReader.read(new StringReader(xml));
-			
+
 			Element root = document.getRootElement();
-			
+
 			String rootName = root.getName();
-			
+
 			Element typeElement = root.element("type");
 			Element userElement = root.element("user");
-			
+
 			assertEquals("message", rootName);
 			assertEquals("1", typeElement.getText());
 			assertEquals("zhangsan", userElement.getText());
-		}
-		catch(Exception ex)
-		{
+		} catch (Exception ex) {
 			ex.printStackTrace();
 			fail();
 		}
