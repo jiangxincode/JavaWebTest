9c9e46597de70655d6b919e94ce09100579d3998
jiangxincode <jiangxinnju@163.com>
2015-10-19 22:34:18 +0800

    add destroy/

---
+++ destroy/src/lee/Chinese.java
@@ -0,0 +1,32 @@
+package lee;
+
+import org.springframework.beans.factory.DisposableBean;
+
+public class Chinese implements Person, DisposableBean {
+	private Axe axe;
+
+	public Chinese() {
+		System.out.println("Spring实例化主调bean：Chinese实例...");
+	}
+
+	public void setAxe(Axe axe) {
+		System.out.println("Spring执行依赖关系注入...");
+		this.axe = axe;
+	}
+
+	public void useAxe() {
+		System.out.println(axe.chop());
+	}
+
+	public void close() {
+		System.out.println("正在执行初始化方法   close...");
+	}
+
+	@Override
+	public void destroy() throws Exception {
+		System.out.println("正在执行初始化方法    destroy...");
+		
+	}
+
+
+}
