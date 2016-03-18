0607e918517c2ab0ecd4d46876e198a76140ed4d
jiangxincode <jiangxinnju@163.com>
2016-03-18 21:23:08 +0800

    jackson example update.

--- src/main/java/edu/jiangxin/json/jackson/Student.java
+++ src/main/java/edu/jiangxin/json/jackson/Student.java
@@ -1,28 +1,33 @@
-package edu.jiangxin.jackson;
+package edu.jiangxin.json.jackson;
 
 import java.util.Date;
 
 public class Student {
-	
+
 	int id;
 	String name;
 	Date birthDay;
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public Date getBirthDay() {
 		return birthDay;
 	}
+
 	public void setBirthDay(Date birthDay) {
 		this.birthDay = birthDay;
 	}
