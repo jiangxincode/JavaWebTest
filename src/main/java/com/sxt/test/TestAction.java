2445711f7216ba3266aadaf1fd082cb82369122a
jiangxincode <jiangxinnju@163.com>
2016-07-20 23:16:00 +0800

    add freemarker related.

---
+++ src/main/java/com/sxt/test/TestAction.java
@@ -0,0 +1,23 @@
+package com.sxt.test;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestAction extends ActionSupport {
+
+	private String companyName="尚学堂";
+
+	public String execute() throws Exception {
+		System.out.println("TestAction.execute()");
+		return SUCCESS;
+	}
+
+	public String getCompanyName() {
+		return companyName;
+	}
+
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+
+
+}
