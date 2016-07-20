edd2a4f1988874b521347695c0de67c7d4ba7be7
jiangxincode <jiangxinnju@163.com>
2016-07-20 23:25:01 +0800

    add time package

--- src/main/java/edu/jiangxin/time/CustomizedTimerTask.java
+++ src/main/java/edu/jiangxin/time/CustomizedTimerTask.java
@@ -1,5 +1,6 @@
 package edu.jiangxin.time;
 
+import java.util.Timer;
 import java.util.TimerTask;
 
 public class CustomizedTimerTask extends TimerTask {
@@ -9,5 +10,21 @@ public class CustomizedTimerTask extends TimerTask {
 	@Override
 	public void run() {
 		System.out.println("The customized TimeTask is invoked. Count: " + (++count));
+	}
+
+	/**
+	 * delay in milliseconds before task is to be executed.
+	 */
+	private static final long DELAY = 1000;
+
+	/**
+	 * period time in milliseconds between successive task executions.
+	 */
+	private static final long PERIOD = 1000;
+
+	public static void main(String[] args) {
+
+		Timer timer = new Timer();
+		timer.schedule(new CustomizedTimerTask(), DELAY, PERIOD);
 	}
 }
