58a4358b986b91d40f2cc30a3ae804e00357172b
Aloys <jiangxinnju@163.com>
2023-05-14 23:16:12 +0800

    add deploy script

---
+++ DeployWindows.ps1
@@ -0,0 +1,9 @@
+$CURRENT_DIR = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$TARGET_DIR = "$CURRENT_DIR/target"
+$RELEASE_JAR_FILE_Filter = 'JavaWebTest-*.war'
+$RELEASE_JAR_FILE = Get-ChildItem -Path "$TARGET_DIR" -Filter $RELEASE_JAR_FILE_Filter
+
+Copy-Item $RELEASE_JAR_FILE.FullName "$TARGET_DIR/JavaWebTest.war"
+
+scp "$TARGET_DIR/JavaWebTest.war" ubuntu@124.222.145.48:/usr/local/tomcat/apache-tomcat-8.5.88/webapps/
+
